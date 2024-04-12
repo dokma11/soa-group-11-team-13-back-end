33ed44ff95e0510333e815207f3649fddd409248
dokma11 <vule.dok@gmail.com>
2024-04-12 20:32:40 +0200

    feat: FollowUserResponseDTO creation

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/FollowUserResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Explorer.Tours.API.Dtos;
+
+public class FollowUserResponseDto
+{
+    public long Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string ProfilePicture { get; set; }
+    public UserRole Role { get; set; }
+    public bool IsActive{ get; set; }
+}
+
+public enum UserRole
+{
+    Administrator, 
+    Author,
+    Tourist
+}
